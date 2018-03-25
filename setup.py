940933ecbcdf6ccdeea0d9dade07a7af52d91859
Sergey Piskunov <sergey.piskunoff@gmail.com>
2018-03-25 11:12:35 -0400

    initial commit

---
+++ setup.py
@@ -0,0 +1,13 @@
+from distutils.core import setup
+setup(
+  name='micropython-hx711',
+  packages=['micropython-hx711'],
+  version='0.1',
+  description='Micropython driver for HX711 24-Bit Analog-to-Digital Converter',
+  author='Sergey Piskunov',
+  author_email='sergey.piskunoff@gmail.com',
+  url='https://github.com/SergeyPiskunov/micropython-hx711',
+  download_url='https://github.com/SergeyPiskunov/micropython-hx711/archive/0.1.tar.gz',
+  keywords=['micropython', 'hx711'],
+  classifiers=[],
+)
