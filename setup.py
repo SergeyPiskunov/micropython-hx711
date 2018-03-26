56318c6f03b766abf792c2ada0caa02384317e1a
Sergey Piskunov <sergey.piskunoff@gmail.com>
2018-03-26 16:44:15 -0400

    minor fixes

--- setup.py
+++ setup.py
@@ -13,18 +13,17 @@ setup(
     py_modules=['hx711'],
     version='1.0.0',
     description='Micropython driver for HX711 24-Bit Analog-to-Digital Converter',
-    long_description='Micropython driver for HX711 24-Bit Analog-to-Digital Converter.',
+    long_description='Micropython driver for HX711 24-Bit Analog-to-Digital Converter',
     keywords=['micropython', 'hx711'],
     url='https://github.com/SergeyPiskunov/micropython-hx711',
-    download_url='https://github.com/SergeyPiskunov/micropython-hx711/archive/1.0.0.tar.gz',
     author='Sergey Piskunov',
     author_email='sergey.piskunoff@gmail.com',
     maintainer='Sergey Piskunov',
     maintainer_email='sergey.piskunoff@gmail.com',
     license='MIT',
-    classifiers = [
+    classifiers=[
         'Development Status :: 4 - Beta',
         'Programming Language :: Python :: Implementation :: MicroPython',
-        'License :: OSI Approved :: MIT License',
+        'License :: MIT License',
     ],
 )
