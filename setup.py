1689ddfd0f8ff8ef5a1674744418d07c574b40cd
cassiorodrigo <cassiorodrigo@gmail.com>
2024-01-16 23:18:51 +0000

    Update setup.py

--- setup.py
+++ setup.py
@@ -9,9 +9,9 @@ import sdist_upip
 
 setup(
     cmdclass={'sdist': sdist_upip.sdist},
-    name='hx711_micropython_impementation_cassiorodrigo',
+    name='hx711_micropython_impementation',
     py_modules=['hx711'],
-    version='1.0.2',
+    version='0.0.1',
     description='Micropython driver for the HX711',
     long_description='Micropython driver for the HX711 24-Bit Analog-to-Digital Converter',
     keywords=['micropython', 'hx711'],
